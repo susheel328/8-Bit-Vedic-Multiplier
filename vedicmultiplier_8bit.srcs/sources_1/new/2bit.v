8221b4118bbee8f17e4b183fd29f54ae13ac18cb
susheel328 <k21susheel@gmail.com>
2025-05-29 20:15:50 +0530

    Add files via upload

---
+++ vedicmultiplier_8bit.srcs/sources_1/new/2bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.05.2025 08:44:03
+// Design Name: 
+// Module Name: 2bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 2bit(
+
+    );
+endmodule
